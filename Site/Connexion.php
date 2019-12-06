ab88a2614522031f547fcb0e35c986890f9e00da
Utilisateur <user@debian.arcahe.ovh>
2019-12-06 12:29:04 +0100

    maj legere

--- Site/Connexion.php
+++ Site/Connexion.php
@@ -6,20 +6,20 @@
 	<link rel="stylesheet" type="text/css" href="style_connexion.css">
 </head>
 <body>
-	<form method="post" action="traitement.php">
+	<form method="post" action="Connexion.php">
 		<div class="formulaire">
 			<p>Connectez-vous</p>
 		</div>
 		<div class="formulaire">
 			<label for="identifiant">Identifiant :</label>
-			<input type="text" id="identifiant" name="id_adherent">
+			<input type="text" id="identifiant" name="id_adherent" required>
 		</div>
 		<div class="formulaire">
 			<label for="mdp">Mot de passe :</label>
-			<input type="password" id="mdp" name="mdp_adherent">
+			<input type="password" id="mdp" name="mdp_adherent" required>
 		</div>
 		<div class="formulaire" id="inscription">
-			<p><a href="Inscription.html" >Pas encore membre ?</a></p>
+			<p><a href="Inscription.php" >Pas encore membre ?</a></p>
 		</div>
 		<div class="formulaire" id="button">
 			<button type="submit" id="envoi">Connexion</button>
--- Site/Inscription.php
+++ Site/Inscription.php
@@ -6,29 +6,30 @@
 	<link rel="stylesheet" type="text/css" href="style_inscription.css">
 </head>
 <body>
-	<form method="post" action="traitement.php">
+	<form method="post" action="Inscription.php">
 		<div class="formulaire">
 			<p>Inscrivez-vous</p>
 		</div>
 		<div class="formulaire">
 			<label for="prenom">Prénom :</label>
-			<input type="text" id="prenom" name="prenom_adherent">
+			<input type="text" id="prenom" name="prenom_adherent" required>
+			
 		</div>
 		<div class="formulaire">
 			<label for="nom">Nom :</label>
-			<input type="text" id="nom" name="nom_adherent">
+			<input type="text" id="nom" name="nom_adherent" required>
 		</div>
 		<div class="formulaire">
 			<label for="email">E-mail :</label>
-			<input type="email" id="email" name="email_adherent">
+			<input type="email" id="email" name="email_adherent" required>
 		</div>
 		<div class="formulaire">
 			<label for="identifiant">Identifiant :</label>
-			<input type="text" id="identifiant" name="id_adherent">
+			<input type="text" id="identifiant" name="id_adherent" required>
 		</div>
 		<div class="formulaire">
 			<label for="mdp">Mot de passe :</label>
-			<input type="password" id="mdp" name="mdp_adherent">
+			<input type="password" id="mdp" name="mdp_adherent" required>
 		</div>
 		<div class="formulaire" id="button">
 			<button type="submit" id="envoi">Inscription</button>
