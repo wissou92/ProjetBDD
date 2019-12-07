7904198898240fe046ae6cbe9c99f298536644a9
moussa <moussa.elhabar@yahoo.fr>
2019-12-07 18:49:25 +0100

    wow quelle page dcceuille woooooooow 20/20 le projet

---
+++ Site/accueil.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>
+		Programmes Sportifs
+	</title>
+	
+	<link rel="stylesheet" type="text/css"  href="style_accueil.css">
+
+<div class="bt">
+<button
+ onclick="window.location.href= 'http://localhost/Projet/Connexion.php';" id ="b2">connexions
+</button>
+<button
+ onclick="window.location.href='http://localhost/Projet/Inscription.php';" id ="b1">inscription
+</button>
+</div> 
+
+
+</head>
+
+<body>
+	<h1>Programes Sportifs</h1>
+
+	<div class = "block">
+	
+
+		<div class= "s1">
+			<h3 id = "t1">CONTACT:</h3>
+	    	<p> <strong> Adresse </strong>: 45 Avenue des États Unis,<br> 78000 Versailles 	<br></br>
+			<strong>Téléphone</strong> : 01 39 25 41 12  <br></br>
+			<strong>Email</strong>: versaille@ens.uvsq.fr </p> 
+		</div>
+  
+	
+		<div class ="s2">
+		   		<h3   id = "t2">CONCEPT: </h3>
+		   		<p> on apprend pas nous ! </p><br> 
+		   	  Faudra inventer une belle histoire <br> 
+		   	  pour la prof <br> 
+		   	  :)
+
+	   	</div>
+
+
+	
+	</div>
+
+
+</body>
+</html>
